21b0fe0503354f26ed8b319b3c1fccd36b504ef2
unknown <haniffaiq95@gmail.com>
2021-08-27 02:00:48 +0700

    mew API

--- app.js
+++ app.js
@@ -2,13 +2,19 @@ import express from 'express';
 const app = express()
 
 const port = 3001
-app.get('/api/notification/add', (req,res)=>{
-    res.json({
+app.get('/api/notification/create', (req,res)=>{
+    res.status(200).json({
         Name : "Hanif"
     })
-    console.log("Nice endpoint");
+    console.log("Nice endpoint for add new");
 });
 
+app.get('/api/notification/update/:id', (req,res)=>{
+    var id = req.params
+    res.status(200).json(req.params)
+    console.log("Nice endpoint for update");
+    console.log(id);
+});
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
